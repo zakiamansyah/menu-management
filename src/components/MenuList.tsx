85ee2f672476656c03a13d86cc004d00cc391555
Zaki Amansyah <zakiamansyah2014@gmail.com>
2025-02-19 00:56:28 +0700

    refactor: change MenuItem children type from MenuItem[] to string[]

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -10,7 +10,7 @@ import { v4 as uuidv4 } from 'uuid';
 interface MenuItem {
   id: string;
   name: string;
-  children: MenuItem[];
+  children: string[];
   depth: number;
   parentId: string | null;
   parentName: string | null;
